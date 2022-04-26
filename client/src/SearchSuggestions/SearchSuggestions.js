9fc34819906dbdbf1c6042821851b41aec899d9e
Jackie <taekwondo.chan@gmail.com>
2022-04-26 22:19:20 +0000

    remove graphql implementation

--- client/src/SearchSuggestions/SearchSuggestions.js
+++ client/src/SearchSuggestions/SearchSuggestions.js
@@ -1,30 +1,8 @@
-import { React } from 'react';
-import { useQuery, gql } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import './SearchSuggestions.css';
 
-const RECIPE_SEARCH_QUERY = gql`
-	query searchSuggestion($terms: String!) {
-		searchRecipes(searchTerm: $terms) {
-			name
-		}
-	}
-`;
-
 const SearchSuggestions = (props) => {
 	const { searchTerm } = props;
-
-	const { data, loading, error } = useQuery(RECIPE_SEARCH_QUERY, {
-		variables: {
-			terms: searchTerm,
-		},
-	});
-
-	if (loading) return '';
-	if (error) {
-		console.log(error);
-		return <pre>{error.message}</pre>;
-	}
 
 	if (data.searchRecipes.length > 0) {
 		console.log(data.searchRecipes);
