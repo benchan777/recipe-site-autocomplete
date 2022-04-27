90fd73ec6cf115d42d79c1687b37948d89623d9e
Jackie <taekwondo.chan@gmail.com>
2022-04-27 00:45:10 +0000

    oh god why is it crashing my computer

--- client/src/SearchSuggestions/SearchSuggestions.js
+++ client/src/SearchSuggestions/SearchSuggestions.js
@@ -1,17 +1,41 @@
+import { React, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SearchSuggestions.css';
 
 const SearchSuggestions = (props) => {
 	const { searchTerm } = props;
+	const [data, setData] = useState([]);
 
-	if (data.searchRecipes.length > 0) {
-		console.log(data.searchRecipes);
+	useEffect(() => {
+		const options = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ prefix: searchTerm }),
+		};
 
-		let searchArr = data.searchRecipes.map(({ name }) => (
+		const fetchData = async () => {
+			try {
+				const response = await fetch('http://127.0.0.1:5000/autocomplete', options);
+				let new_data = await response.json();
+				setData([...new_data.response]);
+			} catch (err) {}
+		};
+
+		if (searchTerm.length >= 1) {
+			fetchData();
+		}
+	});
+
+	if (data.length > 0) {
+		console.log('There are results.');
+		console.log(data);
+		let searchArr = data.map((name) => (
 			<Link to={`/details/${encodeURIComponent(name)}`}>
 				<li>{name}</li>
 			</Link>
 		));
+
+		console.log(searchArr);
 
 		return (
 			<div className="SearchResults" onClick={props.onClick}>
@@ -19,6 +43,7 @@ const SearchSuggestions = (props) => {
 			</div>
 		);
 	} else {
+		console.log(`No matching names`);
 		return <div></div>;
 	}
 };
