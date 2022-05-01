db2952cf11107984a73807beeaa0c1a5ad421844
benchan777 <benchan777@gmail.com>
2022-05-01 06:23:27 +0000

    add and set up CORS

--- server/server.py
+++ server/server.py
@@ -1,17 +1,18 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+from prefix_trie import PrefixTree
 import requests
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def homepage():
-    dict = {
-        "welcome": "hello world!"
-    }
+  dict = {
+    "welcome": "hello world!"
+  }
 
-    return jsonify(dict)
-
+  return jsonify(dict)
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
