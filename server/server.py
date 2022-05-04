596c61422a5ba347cbdb5f0d43bfc77ed50df804
benchan777 <benchan777@gmail.com>
2022-05-04 00:31:11 +0000

    set up autocomplete endpoint & mongodb

--- server/server.py
+++ server/server.py
@@ -1,7 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from dotenv import load_dotenv
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from prefix_trie import PrefixTree
-import requests
+import requests, os
+
+load_dotenv()
+MONGODB_URI = os.getenv('MONGODB_URI')
+MONGODB_NAME = os.getenv('MONGODB_NAME')
+
+client = MongoClient(MONGODB_URI)
+db = client[MONGODB_NAME]
+
+allRecipeNames = []
+# allRecipes = db.recipes.find()
+for recipe in db.recipes.find():
+  allRecipeNames.append(recipe["name"])
+tree = PrefixTree(allRecipeNames)
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +29,16 @@ def homepage():
   }
 
   return jsonify(dict)
+
+@app.route('/autocomplete', methods=['GET', 'POST'])
+def autocomplete():
+  args = request.json
+  prefix = args.get('prefix')
+  print(f'Args: {prefix}')
+  responses = tree.complete(prefix)
+
+  print(responses)
+  return jsonify({"response": responses})
 
 if __name__ == '__main__':
   app.run()
